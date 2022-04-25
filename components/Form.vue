6eb9373552917f1bfd04491bfd2467197a4b0bb6
Mithila91 <Mithila91@users.noreply.github.com>
2022-04-25 18:48:32 +0200

    styling added to filter and form

--- components/Form.vue
+++ components/Form.vue
@@ -29,24 +29,25 @@ const submitHandler = async() => {
 </script>
 <template>
     <div class="w-3/3 mt-20 mb-20 ml-10">
-        <form class="w-32" @submit.prevent="submitHandler">
+        <form @submit.prevent="submitHandler">
             <div>
-                <label for="name">Name</label>
-                <input v-model="formData.name" id="name" type="text" placeholder="name" required class="block mb-5 text-gray-700 border"/>
+                <label for="name" class="font-medium text-m">name</label>
+                <input v-model="formData.name" id="name" type="text" placeholder="name" required class="w-60 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"/>
                
-                <label for="description">Description</label>
-                <input v-model="formData.description" id="description" type="text" placeholder="add description" required class="block mb-5 text-gray-700 border"/>
+                <label for="description" class="font-medium text-m">description</label>
+                <input v-model="formData.description" id="description" type="text" placeholder="add description" required class="w-60 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"/>
         
-                <label for="imageURL">image url</label>
-                <input v-model="formData.image" id="imageURL" type="text" placeholder="Add Image" required class="block mb-5 text-gray-700 border"/>
+                <label for="imageURL" class="font-medium text-m">image url</label>
+                <input v-model="formData.image" id="imageURL" type="text" placeholder="Add Image" required class="w-60 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"/>
                 
-                <label for="vegetarian">Vegeterian:</label>
+                <label for="allergies" class="font-medium text-m">add allergies</label>
+                <input v-model="formData.allergies" type="input" id="allergies" placeholder="add allergies" required class="w-60 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"/>
+
+                 <label for="vegetarian" class="font-medium text-m">vegeterian:</label>
                 <input v-model="formData.vegetarian" type="checkbox" id="vegetarian" class="block mb-5 text-gray-700 border"/>
-                
-                <label for="allergies">add allergies</label>
-                <input v-model="formData.allergies" type="input" id="allergies" placeholder="add allergies" required class="block mb-5 text-gray-700 border"/>
             </div>
-            <Button type="submit">Add to cart</Button>
+            <div class="flex justify-center"><Button class="px-10" type="submit">Add to cart</Button></div>
+            
         </form>
     </div>
 </template>
