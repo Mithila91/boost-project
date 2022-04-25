72b9ef8833b67871344939f09207f0fdfff5a700
Mithila91 <Mithila91@users.noreply.github.com>
2022-04-25 22:49:21 +0200

    small styling in cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -8,9 +8,12 @@ const { cart } = useCart();
             <p class="text-5xl font-bold">cart is empty</p>
         </div>
         <div v-else v-for="(product, index) in cart" :product="product" :key="index">
-            <div class="flex flex-row">
+            <div class="mt-20 flex flex-col justify-center items-center">
                 <img :src="product.image" width="100" height="100" :alt="product.name" class="object-cover h-10 mb-3" loading="lazy">
-                <p>{{product.name}} {{product.quantity}}</p>
+                <span>{{product.name}}</span>
+                 <span>{{product.description}} </span>
+                 <span>Quantity:{{product.quantity}}</span>
+             
             </div>
         </div>
     </div>
