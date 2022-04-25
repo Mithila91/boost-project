66373f3e12721911f9188997c7325aa4afa9e247
Mithila91 <Mithila91@users.noreply.github.com>
2022-04-25 10:28:44 +0200

    added cart product to state

--- composables/state.ts
+++ composables/state.ts
@@ -5,13 +5,26 @@ interface Product {
   image: string
 }
 
+interface CartProduct extends Product {
+  quantity: number
+}
+
 export const useCart = () => {
-  const cart = useState<Product[]>('', () => [])
+  const cart = useState<CartProduct[]>('', () => [])
 
   const addToCart = (product: Product) => {
-    cart.value.push(product)
-    console.log(cart.value)
+    const currentProduct = cart.value.find((p) => p.id === product.id)
+    if (currentProduct) {
+      currentProduct.quantity += 1
+      // const newCart = cart.value.filter((p) => p.id !== product.id)
+      // currentProduct.quantity = +1
+      // newCart.push(currentProduct)
+    } else {
+      const cartProduct: CartProduct = { ...product, quantity: 1 }
+      cart.value.push(cartProduct)
+    }
   }
+
   return {
     cart: readonly(cart),
     addToCart,
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,10 +1,11 @@
 <script setup>
 const { cart } = useCart()
-console.log(cart.value)
+
+//console.log(cart.value)
 </script>
 <template>
-<div v-if="!cart.length"><p>Product not shown</p></div>
-<div v-for="product in cart" :key="index"> 
-<p>{{product.name}}</p>
-</div>
+  <div v-if="!cart.length"><p>Product not shown</p></div>
+  <div v-for="product in cart" :key="index">
+    <p>{{ product.quantity }}</p>
+  </div>
 </template>
